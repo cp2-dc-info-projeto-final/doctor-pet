03576a28d78d910f497368b033952751d8b2ae0d
Ofemann <101880549+Ofemann@users.noreply.github.com>
2022-09-28 12:08:07 -0300

    Add files via upload

--- create.sql
+++ create.sql
@@ -1,1 +1,14 @@
+CREATE USER 'gerente'@'localhost' IDENTIFIED BY '123'; 
+GRANT ALL PRIVILEGES ON ds302.* TO 'gerente'@'localhost';
 
+CREATE TABLE cliente ( 
+    id_cliente int NOT NULL AUTO_INCREMENT,
+    cpf varchar(14) NOT NULL,
+    nome varchar(45) NOT NULL, 
+    email varchar(45) NOT NULL, 
+    nascimento datetime NOT NULL,
+    telefone varchar(20) NOT NULL,
+    endereco varchar(45) NOT NULL,
+    senha varchar (255) NOT NULL,
+    primary key(id_cliente)
+);
