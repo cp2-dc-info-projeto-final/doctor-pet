8e4bee65306f83ef239950047451cd82e43fd603
Ofemann <101880549+Ofemann@users.noreply.github.com>
2022-11-25 17:49:10 -0300

    Update dia 25/11

--- src/servicos/nav.php
+++ src/servicos/nav.php
@@ -1,17 +1,17 @@
 <body>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
- <a class="navbar-brand" href="index.php">
+ <a class="navbar-brand" href="../index.php">
  <img src="../img/logo2.jpg" style="width:200px;">
  </a>
  <ul class="navbar-nav">
  <li class="nav-item">
- <a class="nav-link" href="logout.php">Logout</a>
+ <a class="nav-link" href="../logout.php">Logout</a>
  </li>
  <?php 
     if ($_SESSION["tipo"] == "administrador") {
     ?>
     <li class="nav-item">
-    <a class="nav-link" href="form_extra.php">Gerenciar cadastros</a>
+    <a class="nav-link" href="../form_extra.php">Gerenciar cadastros</a>
     </li>
 <?php
     }
